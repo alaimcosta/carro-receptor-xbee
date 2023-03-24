d58864bc32e6dc56d2d6a7a4f445c79c2e870867
alaim.costa <alaim.costa23@gmail.com>
2023-03-24 12:24:48 -0300

    comentando o código

--- xbeeCarro_receptor_v5.ino
+++ xbeeCarro_receptor_v5.ino
@@ -1,16 +1,19 @@
 //RECEPTOR
+//Importação das bibliotecas a serem utilizadas
 #include<XBee.h>
+//Realizarem comunicação serial com o módulo Xbee
 #include<SoftwareSerial.h>
 
 XBee xbee = XBee();
+//Incicializa um construtor da biblioteca SoftwareSerial
+//Cria um objeto XBEE2, passando as portas 2 e 3 para comunicação serial
 SoftwareSerial XBEE2(2,3);
 
 
 //  --- Mapeamento das portas logicas
-int dado = 0;       //Armazena dados seriais que entram
-//  --- Mapeamento das portas logicas
-int ENA = 5;
-int ENB = 6;
+int dado = 0;//Armazena dados seriais que entram
+int ENA = 5; //porta para ativar motor right
+int ENB = 6; //porta para ativar motor left
 const int IN2 = 4;
 const int IN3 = 7;
 const int IN4 = 8;
@@ -18,49 +21,53 @@ const int IN1 = 9;
 
 // --- Inicialização ---
 void setup() {
+  //definimos o baudrate em 9600
   Serial.begin(9600);  
   for (int i = 5; i < 10; i++) {
-    pinMode(i, OUTPUT);  // inicializa os pinos digital de 5 a 9 como saída
+    pinMode(i, OUTPUT);  // inicializa os pinos digitais de 5 a 9 como saída
   }
+  //Iniciamos a porta XBEE2 responsável por enviar as informações
+  //para o módulo Xbee e definimos o taxa de transmissão(baudrate) de 9600
   XBEE2.begin(9600);
   xbee.begin(Serial);  
-  
 }
 
 void loop() {
+  //verifica se existe algum dado disponível na porta de comunicação serial do Xbee
   if(XBEE2.available() > 0) {
-    char dado = XBEE2.read();  // Lê os dados da porta serial
-    if(dado == '1'){
+    char dado = XBEE2.read();// Lê os dados da porta serial
+    if(dado == '1'){ //se dado for 1 chama a função rotacaoNormal
       rotacaoNormal();
-    } else if (dado == '2') {  // botão acionado
+    } else if (dado == '2') {//se dado for 2 chama a função recuarCarro
       recuarCarro();
-    } else if (dado == '3') {  // botão acionado
+    } else if (dado == '3') {//se dado for 3 chama a função giraRight
       giraRight();
-    } else if (dado == '4') {  // botão acionado
+    } else if (dado == '4') {//se dado for 4 chama a função giraLeft
       giraLeft();
-    } else if(dado == '0'){
+    } else if(dado == '0'){//se dado for 0 chama a função pararMotor
       pararMotor();
     }   
   }
-  Serial.flush();
+  Serial.flush(); //instrução que permite a conclusão da transmissão de dados 
+                  //da porta serial
   }
 
-  
 // --- Funções Auxiliares
-void rotacaoNormal() {  // rotate para frente com menor rotação
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  analogWrite(ENA, 100);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
+void rotacaoNormal() {  // rotate para frente
+  digitalWrite(IN1, HIGH); //motor right
+  digitalWrite(IN2, LOW); //motor right
+  analogWrite(ENA, 100);//Define a velocidade é ativa o motor right. Variar de
+                        // 0 a 255, onde 255 é a velocidade máxima.
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
   analogWrite(ENB, 100);
 }
 void recuarCarro() {  //função para voltar
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
   analogWrite(ENA, 100);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
   analogWrite(ENB, 100);
 }
 void pararMotor() {
@@ -70,16 +77,16 @@ void pararMotor() {
 void giraRight() {  //ROTAÇÃO PARA DIREITA
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  analogWrite(ENA, 100);  //ENA motor lado direito
+  analogWrite(ENA, 100); 
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENB, 100);  //ENB motor lado esquerdo
+  analogWrite(ENB, 100);
 }
 void giraLeft() {  //ROTAÇÃO PARA LEFT
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, 100);  //ENA motor lado direito
+  analogWrite(ENA, 100);  
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENB, 100);  //ENB motor lado esquerdo
+  analogWrite(ENB, 100);  
 }
